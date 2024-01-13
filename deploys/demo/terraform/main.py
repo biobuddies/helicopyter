f352dda701a30810fd6d2792323b047738ea5300
Christopher Covington <cov@mykolab.com>
2024-01-13 17:15:12 -0500

    Complete basic generation

--- deploys/demo/terraform/main.py
+++ deploys/demo/terraform/main.py
@@ -1,8 +1,20 @@
-from cdktf import TerraformVariable, TerraformLocal, TerraformOutput
+from cdktf import LocalExecProvisioner
 from helicopyter import HeliStack
 
 
 def synth(stack: HeliStack):
-    image = TerraformVariable(stack, 'image', type='string')
-    TerraformLocal(stack, 'environment', '${terraform.workspace}')
-    TerraformOutput(stack, 'reference', value=image.to_string())
+    NullResource = stack.load('null_resource')
+
+    stack.Local('codename', stack.codename)
+    stack.Local('environs', '${terraform.workspace}')
+
+    NullResource(
+        'main',
+        provisioners=[
+            LocalExecProvisioner(
+                command='echo $ENVIRONS', environment={'ENVIRONS': '${local.environs}'}, type='local-exec'
+            )
+        ],
+    )
+    hash_ = stack.Variable('hash', type='string')
+    stack.Output('hash', value=hash_.to_string())
