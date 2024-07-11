629b615506bb820d84f890f203635b34b3bca536
Christopher Covington <cov@mykolab.com>
2024-07-11 06:16:42 -0400

    Buddies deploy and mailmap hook (#21)
    ### Background and Links
    * Codifying access and providing another example of using Helicopyter
    are good
    * I've previously used
    https://github.com/jumanjihouse/pre-commit-hooks/blob/master/pre_commit_hooks/check-mailmap
    but
    - I'm trying to cut back on the number of git repositories that
    pre-commit fetches
        - I think a simpler implementation is possible
    
    ### Changes and Testing
    * Use a more unique name for the deploy that grants people access
    ```
    ~/prj/helicopyter$ . includes.sh hta buddies prod
    Generating deploys/buddies/terraform/main.tf.json
    Pushing christopher_covington to cdktf_cdktf_provider_github.membership
    Pushing duncan_tormey to cdktf_cdktf_provider_github.membership
    Pushing james_braza to cdktf_cdktf_provider_github.membership
    Pushing matt_fowler to cdktf_cdktf_provider_github.membership
    github_membership.duncan_tormey: Refreshing state... [id=biobuddies:DuncanTormey]
    github_membership.james_braza: Refreshing state... [id=biobuddies:jamesbraza]
    github_membership.matt_fowler: Refreshing state... [id=biobuddies:mattefowler]
    github_membership.christopher_covington: Refreshing state... [id=biobuddies:covracer]
    
    No changes. Your infrastructure matches the configuration.
    
    Terraform has compared your real infrastructure against your configuration and found no differences, so no changes
    are needed.
    
    Apply complete! Resources: 0 added, 0 changed, 0 destroyed.
    ```
    * Use gsed consistently, in case the expressions are expanded to include
    GNUisms
    * Add a function that can check for missing mailmap entries
    * Fix the `cona` function when VIRTUAL_ENV is set
    * Iterate on the `pre-commit try-repo` tooling
    * Drop the unused `.gitignore.in` and `.pre-commit-config.yaml` files
    * Set the workspace/environment as part of the Terraform plan and apply
    helper functions
    * Stop caching uv as the performance benefit is questionable
    
    ### Questions and Followup
    * Need to switch to OpenTofu
    * Should includes.sh have the executable bit and shebang line set?
    * By the rule of three, a single hook using `.gitignore.in` will become
    better than the two (conflicting) hooks once we get one more combination
    of languages/files.

--- deploys/buddies/terraform/main.py
+++ deploys/buddies/terraform/main.py
@@ -4,16 +4,17 @@ from cdktf_cdktf_provider_github.membership import Membership
 
 from helicopyter import HeliStack
 
-people = {
+buddies = {
     'christopher.covington': ('covracer', 'admin'),
     'duncan.tormey': ('DuncanTormey', 'admin'),
     'james.braza': ('jamesbraza', 'admin'),
+    'matt.fowler': ('mattefowler', 'admin'),
 }
 
 
 def synth(stack: HeliStack) -> None:
     stack.provide('github')
-    for firstname_dot_lastname, (username, role) in people.items():
+    for firstname_dot_lastname, (username, role) in buddies.items():
         stack.push(
             Membership, firstname_dot_lastname.replace('.', '_'), username=username, role=role
         )
