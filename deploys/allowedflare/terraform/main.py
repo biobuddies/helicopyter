205dc18aab81288a47482c969dc50a42a02594fc
Christopher Covington <cov@mykolab.com>
2025-02-18 10:46:12 -0500

    Downgrade

--- deploys/allowedflare/terraform/main.py
+++ deploys/allowedflare/terraform/main.py
@@ -5,7 +5,7 @@ from os import environ
 from cdktf_cdktf_provider_cloudflare.workers_route import WorkersRoute
 from cdktf_cdktf_provider_cloudflare.zero_trust_access_application import (
     ZeroTrustAccessApplication,
-    ZeroTrustAccessApplicationPolicies,
+    ZeroTrustAccessApplicationConfig,
 )
 from cdktf_cdktf_provider_cloudflare.zero_trust_access_identity_provider import (
     ZeroTrustAccessIdentityProvider,
@@ -14,8 +14,6 @@ from cdktf_cdktf_provider_cloudflare.zero_trust_access_identity_provider import 
 from cdktf_cdktf_provider_cloudflare.zero_trust_access_policy import (
     ZeroTrustAccessPolicy,
     ZeroTrustAccessPolicyInclude,
-    ZeroTrustAccessPolicyIncludeAnyValidServiceToken,
-    ZeroTrustAccessPolicyIncludeEmail,
 )
 
 from stacks.base import BaseStack
@@ -34,7 +32,6 @@ def synth(stack: BaseStack) -> None:
         ZeroTrustAccessIdentityProvider,
         'this',
         account_id=account_id,
-        config=ZeroTrustAccessIdentityProviderConfigA(),
         name='One-time PIN',
         type='onetimepin',
     )
@@ -46,36 +43,30 @@ def synth(stack: BaseStack) -> None:
         session_duration='24h',
         skip_interstitial=True,
         policies=[
-            ZeroTrustAccessApplicationPolicies(
-                id=stack.push(
-                    ZeroTrustAccessPolicy,
-                    'email-in',
-                    account_id=account_id,
-                    name='email-in',
-                    decision='allow',
-                    include=[
-                        ZeroTrustAccessPolicyInclude(
-                            email=ZeroTrustAccessPolicyIncludeEmail(email=email)
-                        )
-                    ],
-                ).id,
-                precedence=1,
-            ),
-            ZeroTrustAccessApplicationPolicies(
-                id=stack.push(
-                    ZeroTrustAccessPolicy,
-                    'service-in',
-                    account_id=account_id,
-                    name='service-in',
-                    decision='non_identity',
-                    include=[
-                        ZeroTrustAccessPolicyInclude(
-                            any_valid_service_token=ZeroTrustAccessPolicyIncludeAnyValidServiceToken()
-                        )
-                    ],
-                ).id,
-                precedence=2,
-            ),
+            stack.push(
+                ZeroTrustAccessPolicy,
+                'email-in',
+                account_id=account_id,
+                name='email-in',
+                decision='allow',
+                include=[
+                    ZeroTrustAccessPolicyInclude(
+                        email=[email],
+                    )
+                ],
+            ).id,
+            stack.push(
+                ZeroTrustAccessPolicy,
+                'service-in',
+                account_id=account_id,
+                name='service-in',
+                decision='non_identity',
+                include=[
+                    ZeroTrustAccessPolicyInclude(
+                        any_valid_service_token=True,
+                    )
+                ],
+            ).id,
         ],
     )
 
@@ -83,6 +74,6 @@ def synth(stack: BaseStack) -> None:
         WorkersRoute,
         'this',
         pattern=f'*.{private_domain}/x/*',
-        script='allowedflare-proxy',
+        script_name='allowedflare-proxy',  # script in v5
         zone_id=zone_id,
     )
