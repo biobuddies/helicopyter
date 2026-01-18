c91225edac8179263f6c21acb1789f4826ada6f3
Christopher Covington <cov@mykolab.com>
2026-01-18 15:23:59 -0500

    Add covingtron to Biobuddies; ignore main.tf

--- deploys/buddies/terraform/main.py
+++ deploys/buddies/terraform/main.py
@@ -11,7 +11,7 @@ try:
     )
 except ImportError:
     # Example contents for GitHub Actions and readers
-    mapping = {'christopher.covington': ('covracer', 'admin')}
+    mapping = {'coving.tron': ('covingtron', 'admin')}
 
 
 def synth(stack: BaseStack) -> None:
